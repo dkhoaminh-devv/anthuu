1778513e302bcd5d600b0d81c71f7634023fb7d3
dkhoaminh-devv <dkhoaminh@gmail.com>
2025-10-07 09:17:39 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 const images = [
-    "https://pin.it/5M9bYx0gT",
-    "https://pin.it/BPLxT7E5E",
-    "https://pin.it/4uokgXaBs",
-    "https://pin.it/4VECDw7DG",
-    "https://pin.it/6ExuoI0gw",
-    "https://pin.it/55raP15gg", // ảnh gần cuối
-    "https://pin.it/1gVQI2Ij6"  // ảnh cuối
+    "https://i.ibb.co/7S7c1yR/1.jpg",
+    "https://i.ibb.co/Zm8hJ4M/2.jpg",
+    "https://i.ibb.co/5kYyY0K/3.jpg",
+    "https://i.ibb.co/7nY1M0N/4.jpg",
+    "https://i.ibb.co/tD0f7Pw/5.jpg",
+    "https://i.ibb.co/0KX9q5f/6.jpg", // ảnh gần cuối
+    "https://i.ibb.co/1gVQI2Ij/7.jpg"  // ảnh cuối
 ];
 
 let current = 0;
@@ -14,9 +14,6 @@ const mainImage = document.getElementById("main-image");
 const message = document.getElementById("message");
 const okeBtn = document.getElementById("oke-btn");
 const wrongBtn = document.getElementById("wrong-btn");
-const music = document.getElementById("bg-music");
-
-music.play();
 
 function updateImage(nextIndex) {
     current = nextIndex;
